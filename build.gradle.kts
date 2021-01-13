d59181e5075e9927fc1300affc6e3bbb3adff713
watanuki <watanuki@casareal.co.jp>
2021-01-13 18:39:16 +0900

    domaの不具合対応

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.4.1"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
+
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.spring") version "1.4.21"
+    kotlin("kapt") version "1.3.61"
 }
 
 group = "jp.co.casareal"
@@ -22,10 +24,13 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
     // Spring x Doma
     implementation("org.seasar.doma.boot:doma-spring-boot-starter:1.5.0")
     // Doma
     implementation("org.seasar.doma:doma:2.24.0")
+    kapt("org.seasar.doma:doma:2.24.0")
 
     // PostgewSQL
     implementation("org.postgresql:postgresql")
--- src/main/kotlin/jp/co/casareal/kotlin/KotlinApplication.kt
+++ src/main/kotlin/jp/co/casareal/kotlin/KotlinApplication.kt
@@ -2,10 +2,8 @@ package jp.co.casareal.kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-//@ComponentScan("jp.co.casareal")
 class KotlinApplication
 
 fun main(args: Array<String>) {
